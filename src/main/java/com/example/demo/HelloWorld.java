10973efe74d05f0f8b34a197ede42b8b3033d6cd
tanviagr <tanvia29@gmail.com>
2020-05-22 03:05:27 +0530

    path variables

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,10 @@ public class HelloWorld {
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello world bean!");
 	}
+	
+	@GetMapping(path = "hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable (@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s", name));
+	}
+	
 }
