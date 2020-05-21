b6e9832c5729e2f9479add1f4708a3b49536e46e
tanviagr <tanvia29@gmail.com>
2020-05-22 03:39:39 +0530

    making a User bean and a UserDAO class

--- src/main/java/com/example/demo/helloworld/HelloWorldBean.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldBean.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.helloworld;
 
 public class HelloWorldBean {
 	private String message;
---
+++ src/main/java/com/example/demo/user/UserDAOService.java
@@ -0,0 +1,36 @@
+package com.example.demo.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDAOService {
+	private static List<User> users = new ArrayList<User>();
+	private static int userCount = 3;
+	static {
+		users.add(new User(1, "Adam", new Date()));
+		users.add(new User(2, "Beth", new Date()));
+		users.add(new User(3, "Cynthia", new Date()));
+	}
+	
+	public List<User> findAll() {
+		return users;
+	}
+	
+	public User saveUser(User user) {
+		// whenever you add a new user, the primary key (ID) is calculated by the backend.
+		if (user.getId() == null) user.setId(++userCount);
+		users.add(user);
+		return user;
+	}
+	
+	public User findOne(int id) {
+		for (User user : users) {
+			if (user.getId() == id) return user;
+		}
+		return null;
+	}
+}
