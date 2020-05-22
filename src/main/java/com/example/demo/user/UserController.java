525d6d54f39e672835643bef592d5623de5bd22c
tanviagr <tanvia29@gmail.com>
2020-05-22 20:58:16 +0530

    enhancing the post method to include URI and the status code

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,13 +1,16 @@
 package com.example.demo.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -29,8 +32,13 @@ public class UserController {
 	
 	//create a user
 	@PostMapping(path = "/users")
-	public User createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.saveUser(user);
-		return savedUser;
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
