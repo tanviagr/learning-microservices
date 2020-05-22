9bce883c2311a0e1d43a1b3a2b74b430e5093dfa
tanviagr <tanvia29@gmail.com>
2020-05-22 20:36:20 +0530

    making a post request

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +25,12 @@ public class UserController {
 	@GetMapping(path = "/users/{id}")
 	public User getUser(@PathVariable Integer id) {
 		return service.findOne(id);
+	}
+	
+	//create a user
+	@PostMapping(path = "/users")
+	public User createUser(@RequestBody User user) {
+		User savedUser = service.saveUser(user);
+		return savedUser;
 	}
 }
--- src/main/java/com/example/demo/user/UserDAOService.java
+++ src/main/java/com/example/demo/user/UserDAOService.java
@@ -33,4 +33,5 @@ public class UserDAOService {
 		}
 		return null;
 	}
+	
 }
