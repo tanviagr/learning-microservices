4fa92eabccea0bc706abdc1201436455e7870b90
tanviagr <tanvia29@gmail.com>
2020-05-27 21:57:31 +0530

    Internationalization : some changes to simplify code

--- src/main/java/com/example/demo/helloworld/HelloWorld.java
+++ src/main/java/com/example/demo/helloworld/HelloWorld.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,9 +33,9 @@ public class HelloWorld {
 	
 //	I18n
 	@GetMapping(path = "hello-world-internationalized")
-	public String helloWorldInternational(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+	public String helloWorldInternational() {
 //		return "Good Morning";
-		return messageSource.getMessage("good.morning.message", null, locale);
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	
 }
