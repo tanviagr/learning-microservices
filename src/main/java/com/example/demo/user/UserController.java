c560cf674bcd5c50e53415407fbd2ea469ac09cc
tanviagr <tanvia29@gmail.com>
2020-05-27 17:24:50 +0530

    HATEOAS : adding link to all users in getUser() method

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.net.URI;
 import java.util.List;
 
@@ -7,24 +9,25 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@RequestMapping(path = "/users")
 public class UserController {
 	
 	@Autowired
 	private UserDAOService service;
 	
 	//retrieveAllUsers
-	@GetMapping(path = "/users")
+	@GetMapping(path = "")
 	public List<User> getAllUsers() {
 		List<User> users = service.findAll();
 		if (users.size() == 0) throw new UsersNotFoundException("No users created");
@@ -32,19 +35,21 @@ public class UserController {
 	}
 	
 	//retrieve one user
-	@GetMapping(path = "/users/{id}")
+	@GetMapping(path = "/{id}")
 	public User getUser(@PathVariable Integer id) {
 		User user = service.findOne(id);
 		if (user == null) throw new UserNotFoundException(String.format("id = %s", id));
 //		Link link = new Link("http://localhost:8080/users/1");
 //		The WebMvcLinkBuilder class methods are imported statically
-		Link link = linkTo(UserController.class).slash(user.getId()).withSelfRel();
-		user.add(link);
+		Link selfLink = linkTo(UserController.class).slash(user.getId()).withSelfRel();
+		Link allUsers = linkTo(methodOn(UserController.class).getAllUsers()).withRel("all users");
+		user.add(selfLink);
+		user.add(allUsers);
 		return user;
 	}
 	
 	//create a user
-	@PostMapping(path = "/users")
+	@PostMapping(path = "")
 	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.saveUser(user);
 		URI location = ServletUriComponentsBuilder
@@ -57,7 +62,7 @@ public class UserController {
 	
 	//delete a user
 	//for this, we can either return a no-content ResponseEntity, or return void to signify success at deleting the resource.
-	@DeleteMapping(path = "/users/{id}")
+	@DeleteMapping(path = "/{id}")
 	public ResponseEntity<Object> deleteUser(@PathVariable int id) {
 		User user = service.deleteOne(id);
 		if (user == null) throw new UserNotFoundException("id = " + id);
